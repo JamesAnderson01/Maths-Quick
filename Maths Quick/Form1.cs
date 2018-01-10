6ae0e91ad32b13dff39075ca3d963da9145bf965
RC0030808 <RC0030808@CRO-ICF07-09>
2018-01-10 13:50:21 +0000

    jjj

--- Maths Quick/Form1.cs
+++ Maths Quick/Form1.cs
@@ -24,10 +24,23 @@ namespace Maths_Quick
             double d1 = 3.1 * 2.3;
             double d2 = 1.0 / 3.0;
             double d3 = 1 / 3;
+            double d4 = Math.Sqrt(49);
+            double d5 = Math.Pow(5, 3);
+            double d6 = Math.Pow(27, 1.0 / 3.0);
 
-            richTextBox1.AppendText(i1 + "\n" + i2 + "\n" + i3 + "\n" + i4 + "\n" + i5 + "\n" + i6 + "\n");
-            richTextBox1.AppendText(d1 + "\n" + d2 + "\n" + d3 + "\n");
-            
+        richTextBox1.AppendText(i1 + "\n" + i2 + "\n" + i3 + "\n" + i4 + "\n" + i5 + "\n" + i6 + "\n");
+        richTextBox1.AppendText(d1 + "\n" + d2 + "\n" + d3 + "\n");
+        richTextBox1.AppendText(d4 + "\n" + d5 + "\n" + d6 + "\n");
+
+
+        }
+        double sin (double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+        double asin(double X)
+        {
+            return (Math.Asin(X) * 180 / Math.PI);
         }
 
         private void button1_Click(object sender, EventArgs e)
