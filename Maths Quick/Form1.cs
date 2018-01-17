e3954a9398756915053ced73d70623b484c2e030
RC0030808 <RC0030808@CRO-ICF07-09>
2018-01-17 13:55:29 +0000

    added sin

--- Maths Quick/Form1.cs
+++ Maths Quick/Form1.cs
@@ -27,22 +27,18 @@ namespace Maths_Quick
             double d4 = Math.Sqrt(49);
             double d5 = Math.Pow(5, 3);
             double d6 = Math.Pow(27, 1.0 / 3.0);
-
-        richTextBox1.AppendText(i1 + "\n" + i2 + "\n" + i3 + "\n" + i4 + "\n" + i5 + "\n" + i6 + "\n");
-        richTextBox1.AppendText(d1 + "\n" + d2 + "\n" + d3 + "\n");
-        richTextBox1.AppendText(d4 + "\n" + d5 + "\n" + d6 + "\n");
+            double d7 = sin(160);
+            double d8 = asin(-0.67);
+            double d9 = cos(160);
+            double d10 = acos(-0.67);
+            double d11 = tan(120);
+            double d12 = atan(-0.67);
+            richTextBox1.AppendText(i1 + "\n" + i2 + "\n" + i3 + "\n" + i4 + "\n" + i5 + "\n" + i6 + "\n");
+            richTextBox1.AppendText(d1 + "\n" + d2 + "\n" + d3 + "\n");
+            richTextBox1.AppendText(d4 + "\n" + d5 + "\n" + d6 + "\n" + d7 + "\n" + d8 + "\n" + d9 + "\n" + d10 + "\n" + d11 + "\n" + d12 + "\n");
 
 
         }
-        double sin (double x)
-        {
-            return (Math.Sin(x * Math.PI / 180.0));
-        }
-        double asin(double X)
-        {
-            return (Math.Asin(X) * 180 / Math.PI);
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
 
@@ -52,5 +48,38 @@ namespace Maths_Quick
         {
 
         }
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+        }
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+        double acos(double x)
+        {
+            return (Math.Acos(x) * 180 / Math.PI);
+        }
+        double tan(double x)
+        {
+            return (Math.Tan(x * Math.PI / 180.0));
+        }
+        double atan(double x)
+        {
+            return (Math.Atan(x) * 180 / Math.PI);
+        }
     }
 }
+
+
+
+
+
+
+
+
+            
